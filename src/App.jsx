1337cddbefc00041282c5b99ebe17f72e4ae392e
Mario Kovac <mkovac25122512@gmail.com>
2023-04-22 11:12:34 +0200

    Create basic browser router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+// Routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard"
+import Error from "./pages/Error"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+    errorElement: <Error />
+  },
+])
+
 function App() {
   return (
-    <div className="App">APP!</div>
+    <div className="App"><RouterProvider router={router} /></div>
   )
 }
 
