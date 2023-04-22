ba8e3f9c52e7eb31ce8a192f0c6b761156044269
Mario Kovac <mkovac25122512@gmail.com>
2023-04-22 13:23:18 +0200

    Add basic login functionality

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,37 @@
 // React-router-dom imports
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
+
+// Library imports
+import { toast } from "react-toastify";
+
+// Component imports
+import Intro from "../components/Intro";
 
 // Helper functions
-import { fetchData } from '../helpers'
+import { fetchData } from "../helpers";
 
 // Loader function
 export function dashboardLoader() {
-    const userName = fetchData("userName");
+  const userName = fetchData("userName");
+  return { userName };
+}
 
-    return { userName };
+// Action functions
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}!`);
+  } catch (error) {
+    throw new Error("There was a problem creating your account.");
+  }
 }
 
 const Dashboard = () => {
   const { userName } = useLoaderData();
 
-  return (
-    <div>
-        <h1>{userName}</h1>
-        Dashboard
-    </div>
-  )
-}
+  return <div>{userName ? <p>{userName}</p> : <Intro />}</div>;
+};
 
-export default Dashboard
+export default Dashboard;
